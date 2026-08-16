6c5578a9b07bc8f8d6b80f39d4e8283669c48177
HayaAlwizrah <hayaalwizrah@gmail.com>
2026-08-16 05:25:52 +0300

    Update app.py

--- Project/app.py
+++ Project/app.py
@@ -877,9 +877,5 @@ if __name__ == "__main__":
     print("Press START to activate AI.")
     print("====================================")
 
-    app.run(
-        debug=True,
-        host="0.0.0.0",
-        port=5000,
-        use_reloader=False
-    )
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
